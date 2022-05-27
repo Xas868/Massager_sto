5fa2ddf6c10499d232986c8a971b76c78ce91c40
Игорь <garikleo@yandex.ru>
2022-05-27 22:48:24 +0300

    moderator_id only for admins

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -174,7 +174,7 @@ public class TestDataInitService {
             Answer answer = Answer.builder()
                     .htmlBody("Answer " + i)
                     .user(getRandomUser())
-                    .editModerator(getRandomUser())
+                    .editModerator(getRandomAdmin())
                     .question(getRandomQuestion())
                     .isDeleted(false)
                     .isHelpful(false)
@@ -253,6 +253,13 @@ public class TestDataInitService {
     private User getRandomUser() {
         List<User> users = userService.getAll();
         return users.get(new Random().nextInt(users.size()));
+    }
+    private User getRandomAdmin() {
+         User admin = getRandomUser();
+             if (admin.getRole().getId() == 1) {
+                return admin;
+             }
+        return null;
     }
 
     private Question getRandomQuestion() {
