dc0c18d7bfb47a2f01a4e546ac35b63252080c9f
atihiphob <94682067+atihiphob@users.noreply.github.com>
2022-07-02 15:20:29 +0300

    get all messages of single chat by single chat id v3

--- src/main/java/com/javamentor/qa/platform/dao/impl/pagination/messagedto/MessagePageDtoBySingleChatId.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/pagination/messagedto/MessagePageDtoBySingleChatId.java
@@ -18,6 +18,7 @@ public class MessagePageDtoBySingleChatId implements PageDtoDao<MessageDto> {
 
     @Override
     public List<MessageDto> getPaginationItems(PaginationData properties) {
+        int itemsOnPage = properties.getItemsOnPage();
         long singleChatID = (long) properties.getProps().get("singleChatId");
         return entityManager.createQuery("select new com.javamentor.qa.platform.models.dto.MessageDto " +
                 "(message.id, " +
@@ -29,6 +30,8 @@ public class MessagePageDtoBySingleChatId implements PageDtoDao<MessageDto> {
                 "from Message as message inner join SingleChat as single_chat on message.chat.id = single_chat.chat.id " +
                 "where single_chat.id = :singleChatId order by message.persistDate desc", MessageDto.class)
                 .setParameter("singleChatId", singleChatID)
+                .setFirstResult((properties.getCurrentPage() - 1) * itemsOnPage)
+                .setMaxResults(itemsOnPage)
                 .getResultList();
     }
 
