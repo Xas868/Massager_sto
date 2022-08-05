92e39ea3baab3a7a6bbb135097fe8d445d3d4b4b
SergeyZotoff <dart-adwokat@yandex.ru>
2022-08-05 12:03:24 +0500

    Добавил страницу неотвеченных вопросов, сделал редирект на нее

--- src/main/java/com/javamentor/qa/platform/dao/impl/pagination/QuestionPageDtoDaoAllQuestionsImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/pagination/QuestionPageDtoDaoAllQuestionsImpl.java
@@ -25,29 +25,24 @@ public class QuestionPageDtoDaoAllQuestionsImpl implements PageDtoDao<QuestionVi
         int itemsOnPage = properties.getItemsOnPage();
         int offset = (properties.getCurrentPage() - 1) * itemsOnPage;
         return (List<QuestionViewDto>) entityManager.createQuery(
-                "SELECT DISTINCT " +
-                        "question.id, " +
-                        "question.title, " +
-                        "user.id, " +
-                        "user.fullName, " +
-                        "user.imageLink, " +
-                        "question.description, " +
-                        "question.persistDateTime," +
-                        "question.lastUpdateDateTime," +
-
-                        "(SELECT SUM(reputation.count) from Reputation reputation WHERE reputation.author.id = question.user.id), " +
-                        "(coalesce((select count(answer.id) from Answer answer where answer.question.id = question.id),0)) as answerCounter, " +
-                        "(coalesce((select sum(case when v.vote = 'UP_VOTE' then 1 else -1 end) from VoteQuestion v where v.question.id = question.id), 0))," +
-                        "(select count(bookmarks.id) from BookMarks bookmarks where bookmarks.question.id= question.id and bookmarks.user.id=:userId ) " +
-
-                        "from Question question " +
-
-                        "JOIN User user on question.user.id = user.id " +
-
-                        "where " +
-                        "((:trackedTags) IS NULL OR question.id IN (select question.id from Question question join question.tags t where t.id in (:trackedTags))) and " +
-                        "((:ignoredTags) IS NULL OR question.id not IN (select question.id from Question question join question.tags t where t.id in (:ignoredTags))) and " +
-                        " :dateFilter = 0  OR question.persistDateTime >= current_date - :dateFilter ")
+                        "SELECT DISTINCT " +
+                                " q.id as question_id," +
+                                " q.title," +
+                                " q.user.id as author_id," +
+                                " q.user.fullName," +
+                                " q.user.imageLink," +
+                                " q.description," +
+                                " q.persistDateTime," +
+                                " q.lastUpdateDateTime," +
+                                " (select sum(r.count) from Reputation r where r.author.id = q.user.id) as author_reputation," +
+                                " (coalesce((select count(a.id) from Answer a where a.question.id = q.id),0)) as answerCounter, " +
+                                " (coalesce((select sum(case when v.vote = 'UP_VOTE' then 1 else -1 end) from VoteQuestion v where v.question.id = q.id), 0)) as count_valuable," +
+                                " (select count(bm.id) > 0 from BookMarks bm where bm.question.id = q.id and bm.user.id = :userId) as is_user_bookmark, " +
+                                " (coalesce((select count(qv.id) from QuestionViewed qv where qv.question.id = q.id), 0)) as view_count" +
+                                " from Question q" +
+                                " where ((:trackedTags) IS NULL OR q.id IN (select q.id from Question q join q.tags t where t.id in (:trackedTags))) " +
+                                " and ((:ignoredTags) IS NULL OR q.id not IN (select q.id from Question q join q.tags t where t.id in (:ignoredTags))) and " +
+                        " :dateFilter = 0  OR q.persistDateTime >= current_date - :dateFilter ")
 
                 .setParameter("trackedTags", properties.getProps().get("trackedTags"))
                 .setParameter("ignoredTags", properties.getProps().get("ignoredTags"))
