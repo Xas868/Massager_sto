109268ace87a6b63c9d95252b8e1444513b9bb75
Yurfff <sidash2002@gmail.com>
2022-08-30 17:00:53 +0300

    Исправил API, исправил тесты

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/MessageResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/MessageResourceController.java
@@ -6,11 +6,7 @@ import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.model.MessageService;
 import com.javamentor.qa.platform.service.abstracts.model.MessageStarService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.callbacks.Callback;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Tag(name = "MessageResourceController", description = "Позволяет работать с избранными сообщениями")
 @RestController
@@ -42,22 +40,19 @@ public class MessageResourceController {
             @Content(mediaType = "application/json")
     })
     @PostMapping("/star")
-    public ResponseEntity<?> addMessageToStarMessages(@Parameter(schema = @Schema(example = "{" +
-            "\"id\":1" +
-            "}")) @RequestBody Message messageToStar) {
+    public ResponseEntity<?> addMessageToStarMessages(@RequestBody Long messageId) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) auth.getPrincipal();
-        Message messageFromDB;
-        try {
-            messageFromDB = messageService.getById(messageToStar.getId()).get();
-        } catch (Exception e) {
+        Optional<Message> messageToStar = messageService.getById(messageId);
+        if (messageToStar.isPresent()) {
+            MessageStar message = new MessageStar();
+            message.setUser(user);
+            message.setMessage(messageToStar.get());
+            messageStarService.persist(message);
+        } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        MessageStar message = new MessageStar();
-        message.setUser(user);
-        message.setMessage(messageFromDB);
-        messageStarService.persist(message);
-        return new ResponseEntity<>("Message with id = " + messageToStar.getId() + " was successfully add to stars", HttpStatus.OK);
+        return new ResponseEntity<>("Message with id = " + messageId + " was successfully add to stars", HttpStatus.OK);
     }
 
 }
--- src/test/java/com/javamentor/qa/platform/api/TestMessageResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestMessageResourceController.java
@@ -1,6 +1,7 @@
 package com.javamentor.qa.platform.api;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.core.api.dataset.SeedStrategy;
 import com.javamentor.qa.platform.AbstractClassForDRRiderMockMVCTests;
 import org.junit.jupiter.api.Test;
@@ -16,24 +17,31 @@ public class TestMessageResourceController extends AbstractClassForDRRiderMockMV
             value = {"dataset/MessageResourceController/users.yml",
                     "dataset/MessageResourceController/messages.yml"},
             strategy = SeedStrategy.REFRESH)
+    @ExpectedDataSet(value = "dataset/MessageResourceController/expected/message_star.yml")
     public void testAddMessageToStarMessagesMethod() throws Exception {
         String token = getToken("user1@mail.ru", "user1");
-        // Попытка отправить пустой пост запрос - ожидаемый статус: 400
+        // Попытка отправить пустой пост запрос
         mockMvc.perform(post(TEST_URL)
                 .contentType("application/json")
                 .header("Authorization", "Bearer " + token))
                 .andExpect(status().isBadRequest());
-        // Попытка отправить пост запрос с некорректным полем(сообщение с ID 1000 не существует) - ожидаемый статус: 400
+        // Попытка отправить пост запрос с некорректным полем(сообщение с ID 1000 не существует)
         mockMvc.perform(post(TEST_URL)
                 .contentType("application/json")
                 .header("Authorization", "Bearer " + token)
-                .content("{\"id\":1000}"))
+                .content("1000"))
                 .andExpect(status().isBadRequest());
-        // Попытка отправить корректный пост запрос(сообщение с ID 1 существует) - ожидаемый статус: 200
+        // Попытка отправить корректный пост запрос(сообщение с ID 1 существует)
         mockMvc.perform(post(TEST_URL)
                 .contentType("application/json")
                 .header("Authorization", "Bearer " + token)
-                .content("{\"id\":1}"))
+                .content("1"))
                 .andExpect(status().isOk());
+        // Попытка отправить что-то что не число
+        mockMvc.perform(post(TEST_URL)
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + token)
+                .content("string"))
+                .andExpect(status().isBadRequest());
     }
 }
