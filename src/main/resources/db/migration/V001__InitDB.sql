b0e62817afa759fcc82aaca9180437b9cddc8bce
Yurfff <sidash2002@gmail.com>
2022-09-01 04:27:45 +0300

    Добавил сущность, добавил миграцию

--- src/main/resources/db/migration/V001__InitDB.sql
+++ src/main/resources/db/migration/V001__InitDB.sql
@@ -16,6 +16,7 @@ create sequence user_seq start 1 increment 1;
 create sequence user_badges_seq start 1 increment 1;
 create sequence user_favorite_question_seq start 1 increment 1;
 create sequence vote_question_seq start 1 increment 1;
+create sequence user_chat_pin_seq start 1 increment 1;
 
 create table answer (
                         id int8 not null,
@@ -231,6 +232,14 @@ create table votes_on_questions (
                                     primary key (id)
 );
 
+create table user_chat_pin (
+    id bigint not null,
+    chat_id bigint not null,
+    user_id bigint not null,
+    persist_date timestamp,
+    primary key (id)
+);
+
 alter table answer
     add constraint answer_question_fk
         foreign key (question_id)
@@ -435,3 +444,13 @@ alter table votes_on_questions
     add constraint votes_on_questions_user_fk
         foreign key (user_id)
             references user_entity;
+
+alter table user_chat_pin
+    add constraint user_chat_pin_user_fk
+        foreign key (user_id)
+            references user_entity;
+
+alter table user_chat_pin
+    add constraint user_chat_pin_chat_fk
+        foreign key (chat_id)
+            references chat;
