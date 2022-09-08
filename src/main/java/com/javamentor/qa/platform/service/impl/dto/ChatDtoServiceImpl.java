3351ae0d39625c2dcf5f34357d09a2759164de7d
Roman <safonov.roman.86@yandex.ru>
2022-09-08 22:06:12 +1000

    Tested, working

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/ChatDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/ChatDtoServiceImpl.java
@@ -53,7 +53,11 @@ public class ChatDtoServiceImpl extends DtoServiceImpl<MessageDto> implements Ch
                 // Сортируем сперва закрепленные чаты по времени последнего сообщения, затем также незакрепленные
                 .sorted((ChatDto chat1, ChatDto chat2) -> {
                     if (chat1.isChatPin()) {
-                        return -1;
+                        if (chat2.isChatPin()) {
+                            return chat2.getPersistDateTimeLastMessage().compareTo(chat1.getPersistDateTimeLastMessage());
+                        } else {
+                            return -1;
+                        }
                     } else if (chat2.isChatPin()) {
                         return 1;
                     } else {
