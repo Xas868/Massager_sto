6230977329887fdc909719e402db0f1ed137fb9e
IrinaM83 <irochek-mikh89@mail.ru>
2022-11-01 09:31:16 +0300

    edit TestCommentResourceController

--- src/test/java/com/javamentor/qa/platform/api/TestCommentResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestCommentResourceController.java
@@ -2,10 +2,8 @@ package com.javamentor.qa.platform.api;
 
 import com.javamentor.qa.platform.AbstractClassForDRRiderMockMVCTests;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -14,13 +12,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class TestCommentResourceController extends AbstractClassForDRRiderMockMVCTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     // Все хорошо комментарий к вопросу создан
     @Test
-    @Sql("/script/Before.sql")
-    @Sql(scripts = "/script/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql("/script/TestCommentResourceController/shouldAddCommentQuestion/Before.sql")
+    @Sql(scripts = "/script/TestCommentResourceController/shouldAddCommentQuestion/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void shouldAddCommentQuestion() throws Exception {
         mockMvc.perform(post("/api/user/comment/question/{questionId}", 102)
                         .content("text comment")
@@ -37,26 +32,26 @@ public class TestCommentResourceController extends AbstractClassForDRRiderMockMV
 
     // Пользователь передает неверный questionId
     @Test
-    @Sql("/script/Before.sql")
-    @Sql(scripts = "/script/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql("/script/TestCommentResourceController/shouldError400UserSendInvalidQuestionId/Before.sql")
+    @Sql(scripts = "/script/TestCommentResourceController/shouldError400UserSendInvalidQuestionId/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void shouldError400UserSendInvalidQuestionId() throws Exception {
-        mockMvc.perform(post("/api/user/comment/question/{questionId}", 103)
+        mockMvc.perform(post("/api/user/comment/question/{questionId}", 150)
                         .content("text comment")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + getToken("user102@mail.ru", "user1")))
+                        .header("Authorization", "Bearer " + getToken("user103@mail.ru", "user1")))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
 
     // Пользователь пытается передать пустой комментарий
     @Test
-    @Sql("/script/Before.sql")
-    @Sql(scripts = "/script/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql("/script/TestCommentResourceController/shouldUserSendEmptyComment/Before.sql")
+    @Sql(scripts = "/script/TestCommentResourceController/shouldUserSendEmptyComment/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void shouldUserSendEmptyComment() throws Exception {
         mockMvc.perform(post("/api/user/comment/question/{questionId}", 102)
                         .content("")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + getToken("user102@mail.ru", "user1")))
+                        .header("Authorization", "Bearer " + getToken("user104@mail.ru", "user1")))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
--- src/test/resources/script/After.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM comment_question where comment_id = 1;
-DELETE FROM question WHERE id = 102;
-DELETE FROM comment WHERE id = 1;
-DELETE FROM user_entity WHERE id = 102;
-DELETE FROM role WHERE id = 3;
