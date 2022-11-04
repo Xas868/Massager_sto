32ad411e3d084c55415ef1a3d57109651a0f68b9
uskelenov <uskelenovdeal@gmail.com>
2022-11-04 14:53:04 +0300

    Исправил CommentResourceController: вернул api POST /question/{questionId} и добавил api GET /answer/{answerId}.

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/CommentResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/CommentResourceController.java
@@ -4,7 +4,12 @@ import com.javamentor.qa.platform.dao.impl.pagination.commentdto.AnswerCommentPa
 import com.javamentor.qa.platform.models.dto.AnswerCommentDto;
 import com.javamentor.qa.platform.models.dto.PageDTO;
 import com.javamentor.qa.platform.models.entity.pagination.PaginationData;
+import com.javamentor.qa.platform.models.entity.question.CommentQuestion;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.CommentDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.CommentQuestionService;
+import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -13,7 +18,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Tag(name = "CommentResourceController", description = "Позволяет работать с комментариями")
 @RestController
@@ -21,10 +29,39 @@ import org.springframework.web.bind.annotation.*;
 public class CommentResourceController {
 
     private final CommentDtoService commentDtoService;
+    private final QuestionService questionService;
+    private final CommentQuestionService commentQuestionService;
 
     @Autowired
-    public CommentResourceController(CommentDtoService commentDtoService) {
+    public CommentResourceController(QuestionService questionService, CommentQuestionService commentQuestionService,
+                                     CommentDtoService commentDtoService) {
+        this.questionService = questionService;
+        this.commentQuestionService = commentQuestionService;
         this.commentDtoService = commentDtoService;
+    }
+
+    @Operation(
+            summary = "Добавление комментария в вопрос",
+            description = "Добавление комментария в вопрос"
+    )
+    @ApiResponse(responseCode = "200", description = "Комментарий добавлен", content = {
+            @Content(mediaType = "application/json")
+    })
+    @ApiResponse(responseCode = "400", description = "Комментарий не добавлен", content = {
+            @Content(mediaType = "application/json")
+    })
+    @PostMapping("/question/{questionId}")
+    public ResponseEntity<?> addCommentQuestion(@PathVariable Long questionId, @RequestBody String textComment,
+                                                Authentication auth){
+        User user = (User) auth.getPrincipal();
+        Optional<Question> question = questionService.getById(questionId);
+        if (question.isEmpty()) {
+            return new ResponseEntity<>("There is not question " + questionId.toString(), HttpStatus.BAD_REQUEST);
+        }
+        CommentQuestion commentQuestion = new CommentQuestion(textComment, user);
+        commentQuestion.setQuestion(question.get());
+        commentQuestionService.persist(commentQuestion);
+        return new ResponseEntity<>("Comment successfully added", HttpStatus.OK);
     }
 
     @GetMapping("/answer/{answerId}")
