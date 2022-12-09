dc82b8c14f90b2eca56118469b9b3abb556d14a2
Nurbolot <ngulamidinov@gmail.com>
2022-12-10 04:17:45 +0600

    Added the ability to sort by votes, by viewing, and by the date of adding

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
@@ -7,14 +7,15 @@ import com.javamentor.qa.platform.models.dto.PageDTO;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.models.dto.UserProfileQuestionDto;
 import com.javamentor.qa.platform.models.entity.pagination.PaginationData;
+import com.javamentor.qa.platform.models.entity.question.ProfileQuestionSort;
+import com.javamentor.qa.platform.models.entity.question.comparator.DateComparator;
+import com.javamentor.qa.platform.models.entity.question.comparator.ViewComparator;
+import com.javamentor.qa.platform.models.entity.question.comparator.VoteComparator;
 import com.javamentor.qa.platform.models.util.CalendarPeriod;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -38,23 +39,35 @@ public class UserDtoServiceImpl extends DtoServiceImpl<UserDto> implements UserD
 
     @Override
     public List<UserProfileQuestionDto> getUserProfileQuestionDtoByUserIdIsDeleted(Long id) {
-        List<UserProfileQuestionDto> resultList=userDtoDao.getAllUserProfileQuestionDtoByUserIdWhereQuestionIsDeleted(id);
+        List<UserProfileQuestionDto> resultList = userDtoDao.getAllUserProfileQuestionDtoByUserIdWhereQuestionIsDeleted(id);
         var map = tagDtoDao.getTagDtoByQuestionIds(
                 resultList.stream().map(UserProfileQuestionDto::getQuestionId).collect(Collectors.toList())
         );
-        resultList.forEach(q -> q.setListTagDto(map.containsKey(q.getQuestionId())?map.get(q.getQuestionId()):new ArrayList<>()));
+        resultList.forEach(q -> q.setListTagDto(map.containsKey(q.getQuestionId()) ? map.get(q.getQuestionId()) : new ArrayList<>()));
         return resultList;
     }
 
     @Override
     public List<UserProfileQuestionDto> getAllUserProfileQuestionDtoById(Long id) {
-        List<UserProfileQuestionDto> resultList=userDtoDao.getAllUserProfileQuestionDtoById(id);
+        List<UserProfileQuestionDto> resultList = userDtoDao.getAllUserProfileQuestionDtoById(id);
         var map = tagDtoDao.getTagDtoByQuestionIds(
                 resultList.stream().map(UserProfileQuestionDto::getQuestionId).collect(Collectors.toList())
         );
         resultList.forEach(q ->
-                q.setListTagDto(map.containsKey(q.getQuestionId())?map.get(q.getQuestionId()):new ArrayList<>()));
+                q.setListTagDto(map.containsKey(q.getQuestionId()) ? map.get(q.getQuestionId()) : new ArrayList<>()));
         return resultList;
+    }
+
+    @Override
+    public List<UserProfileQuestionDto> getAllUserProfileQuestionDtoByIdAndSort(Long id, ProfileQuestionSort profileQuestionSort) {
+        Comparator<UserProfileQuestionDto> comparator = (
+                profileQuestionSort.equals(ProfileQuestionSort.VOTE) ? new VoteComparator()
+                        : profileQuestionSort.equals(ProfileQuestionSort.VIEW) ? new ViewComparator()
+                        : new DateComparator());
+
+        return getAllUserProfileQuestionDtoById(id).stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
     }
 
     @Override
