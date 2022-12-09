61b853111012af64f2f96a584bd2cfeb6be09ff5
Nurbolot <ngulamidinov@gmail.com>
2022-12-10 04:11:18 +0600

    add new fields

--- src/main/java/com/javamentor/qa/platform/models/dto/UserProfileQuestionDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/UserProfileQuestionDto.java
@@ -1,9 +1,13 @@
 package com.javamentor.qa.platform.models.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
