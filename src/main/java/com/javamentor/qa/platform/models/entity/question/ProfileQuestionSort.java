2d7a9548399763ecc234493a3f2b77cc882f6a6d
Nurbolot <ngulamidinov@gmail.com>
2022-12-12 20:22:55 +0600

    updated ProfileQuestionSort added lombok code

--- src/main/java/com/javamentor/qa/platform/models/entity/question/ProfileQuestionSort.java
+++ src/main/java/com/javamentor/qa/platform/models/entity/question/ProfileQuestionSort.java
@@ -4,20 +4,18 @@ import com.javamentor.qa.platform.models.dto.UserProfileQuestionDto;
 import com.javamentor.qa.platform.models.entity.question.comparator.DateComparator;
 import com.javamentor.qa.platform.models.entity.question.comparator.ViewComparator;
 import com.javamentor.qa.platform.models.entity.question.comparator.VoteComparator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Comparator;
 
+@RequiredArgsConstructor
+@Getter
+@AllArgsConstructor
 public enum ProfileQuestionSort {
     NEW(new DateComparator()),
     VIEW(new ViewComparator()),
     VOTE(new VoteComparator());
     private Comparator<UserProfileQuestionDto> comparator;
-
-    ProfileQuestionSort(Comparator<UserProfileQuestionDto> comparator) {
-        this.comparator = comparator;
-    }
-
-    public Comparator<UserProfileQuestionDto> getComparator() {
-        return comparator;
-    }
 }
