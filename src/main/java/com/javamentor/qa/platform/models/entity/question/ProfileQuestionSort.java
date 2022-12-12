f3f7f2dba4867965d740a719b0aaafe6ca9f948f
Nurbolot <ngulamidinov@gmail.com>
2022-12-12 19:57:11 +0600

    updated ProfileQuestionSort add field comparator

--- src/main/java/com/javamentor/qa/platform/models/entity/question/ProfileQuestionSort.java
+++ src/main/java/com/javamentor/qa/platform/models/entity/question/ProfileQuestionSort.java
@@ -1,4 +1,23 @@
 package com.javamentor.qa.platform.models.entity.question;
+
+import com.javamentor.qa.platform.models.dto.UserProfileQuestionDto;
+import com.javamentor.qa.platform.models.entity.question.comparator.DateComparator;
+import com.javamentor.qa.platform.models.entity.question.comparator.ViewComparator;
+import com.javamentor.qa.platform.models.entity.question.comparator.VoteComparator;
+
+import java.util.Comparator;
+
 public enum ProfileQuestionSort {
-    NEW, VIEW, VOTE;
+    NEW(new DateComparator()),
+    VIEW(new ViewComparator()),
+    VOTE(new VoteComparator());
+    private Comparator<UserProfileQuestionDto> comparator;
+
+    ProfileQuestionSort(Comparator<UserProfileQuestionDto> comparator) {
+        this.comparator = comparator;
+    }
+
+    public Comparator<UserProfileQuestionDto> getComparator() {
+        return comparator;
+    }
 }
