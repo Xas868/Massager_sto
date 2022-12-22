efdf9aaf9b3112b460a2d2750bd1bcc294608b34
Nurbolot <ngulamidinov@gmail.com>
2022-12-22 22:17:16 +0600

    fixed bugs

--- src/main/java/com/javamentor/qa/platform/models/entity/GroupBookmark.java
+++ src/main/java/com/javamentor/qa/platform/models/entity/GroupBookmark.java
@@ -1,25 +1,22 @@
 package com.javamentor.qa.platform.models.entity;
 
 import com.javamentor.qa.platform.models.entity.user.User;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Objects;
 import java.util.Set;
 
@@ -29,41 +26,31 @@ import java.util.Set;
 @Builder
 @Table(name = "group_bookmark")
 public class GroupBookmark {
-	@Getter(AccessLevel.NONE)
-	@Setter(AccessLevel.NONE)
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	@Column(name = "id", nullable = false)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-	private String title;
-	@ManyToMany(fetch = FetchType.LAZY)
-	@ToString.Exclude
-	@JoinTable(name = "bookmark_has_group")
-	private Set<BookMarks> bookMarks;
+    private String title;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinTable(name = "bookmark_has_group")
+    private Set<BookMarks> bookMarks;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@ToString.Exclude
-	private User user;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ToString.Exclude
+    private User user;
 
-	public Long getId() {
-		return id;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        GroupBookmark that = (GroupBookmark) o;
+        return id != null && Objects.equals(id, that.id);
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-		GroupBookmark that = (GroupBookmark) o;
-		return id != null && Objects.equals(id, that.id);
-	}
-
-	@Override
-	public int hashCode() {
-		return getClass().hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
