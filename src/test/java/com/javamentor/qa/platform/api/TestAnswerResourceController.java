3996d791d2080e68a217335713730a9259742a75
xenklao <109021940+xenklao@users.noreply.github.com>
2022-12-25 18:25:17 +0500

    Initial commit

--- src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
@@ -1,4 +1,37 @@
 package com.javamentor.qa.platform.api;
 
-public class TestAnswerResourceController {
+import com.javamentor.qa.platform.AbstractClassForDRRiderMockMVCTests;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TestAnswerResourceController extends AbstractClassForDRRiderMockMVCTests {
+
+    //Смена пароля
+    @Test
+    @Sql(scripts = "/script/TestUserResourceController/changePassword/Before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestUserResourceController/changePassword/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void changePassword() throws Exception {
+
+        mockMvc.perform(patch("/api/user/change/password")
+                        .param("password", "46xEPoAOu")
+                        .contentType("application/json")
+                        .header("Authorization", "Bearer " + getToken("user100@mail.ru", "user100")))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+//        String user = String.valueOf(entityManager.createQuery("select password from User where id = 100")
+//                .getResultList());
+//        user = user.replaceAll("[()<\\[\\]>]","");
+//
+//        Boolean bool = passwordEncoder.matches((CharSequence) "46xEPoAOu", user);
+//
+//        assertThat(bool).isEqualTo(true);
+
+
+    }
 }
