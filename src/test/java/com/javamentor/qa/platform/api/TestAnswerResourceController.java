d1bd4d8a859e15300c8f1c385b16fb3d98b7ec8c
xenklao <109021940+xenklao@users.noreply.github.com>
2022-12-25 18:28:12 +0500

    Initial commit

--- src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
@@ -11,13 +11,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class TestAnswerResourceController extends AbstractClassForDRRiderMockMVCTests {
 
-    //Смена пароля
+    //Голосование за
     @Test
     @Sql(scripts = "/script/TestUserResourceController/changePassword/Before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/script/TestUserResourceController/changePassword/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void changePassword() throws Exception {
+    void upVote() throws Exception {
 
-        mockMvc.perform(patch("/api/user/change/password")
+        mockMvc.perform(patch("/api/user/question/{questionId}/answer/1/upVote")
                         .param("password", "46xEPoAOu")
                         .contentType("application/json")
                         .header("Authorization", "Bearer " + getToken("user100@mail.ru", "user100")))
