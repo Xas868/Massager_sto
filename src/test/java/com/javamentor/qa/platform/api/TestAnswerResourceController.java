c3bfa20c5414d230da44eafff8ecb8c3e1bff8fb
xenklao <109021940+xenklao@users.noreply.github.com>
2022-12-26 22:03:46 +0500

    Up and Down commit

--- src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
@@ -1,12 +1,15 @@
 package com.javamentor.qa.platform.api;
 
 import com.javamentor.qa.platform.AbstractClassForDRRiderMockMVCTests;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class TestAnswerResourceController extends AbstractClassForDRRiderMockMVCTests {
@@ -17,12 +20,13 @@ public class TestAnswerResourceController extends AbstractClassForDRRiderMockMVC
     @Sql(scripts = "/script/TestAnswerResourceController/upVote/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void upVote() throws Exception {
 //        {questionId}
-        mockMvc.perform(patch("/api/user/question/101/answer/101/upVote")
+        mockMvc.perform(post("/api/user/question/101/answer/101/upVote")
 //                        .param("password", "46xEPoAOu")
                         .contentType("application/json")
-                        .header("Authorization", "Bearer " + getToken("user102@mail.ru", "user102")))
+                        .header("Authorization", "Bearer " + getToken("user101@mail.ru", "user101")))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Is.is(1)));
 
 //        String user = String.valueOf(entityManager.createQuery("select password from User where id = 100")
 //                .getResultList());
@@ -31,6 +35,22 @@ public class TestAnswerResourceController extends AbstractClassForDRRiderMockMVC
 //        Boolean bool = passwordEncoder.matches((CharSequence) "46xEPoAOu", user);
 //
 //        assertThat(bool).isEqualTo(true);
+
+
+    }
+
+    //Голосование за
+    @Test
+    @Sql(scripts = "/script/TestAnswerResourceController/downVote/Before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestAnswerResourceController/downVote/After.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void downVote() throws Exception {
+
+        mockMvc.perform(post("/api/user/question/101/answer/101/downVote")
+                        .contentType("application/json")
+                        .header("Authorization", "Bearer " + getToken("user101@mail.ru", "user101")))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Is.is(-1)));
 
 
     }
