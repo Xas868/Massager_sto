49e89a3c392f95933933cbe3323d7fba4636ba61
xenklao <109021940+xenklao@users.noreply.github.com>
2023-01-25 21:12:01 +0500

    Controller

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserBlockedResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserBlockedResourceController.java
@@ -1,8 +1,8 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
 
-import com.javamentor.qa.platform.models.entity.bookmark.BookMarks;
 import com.javamentor.qa.platform.models.entity.user.User;
+
 import com.javamentor.qa.platform.service.abstracts.model.UserBlockedService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/user/")
 public class UserBlockedResourceController {
@@ -22,6 +20,10 @@ public class UserBlockedResourceController {
 
 
     private final UserBlockedService userBlockedService;
+
+    public UserBlockedResourceController(UserBlockedService userBlockedService) {
+        this.userBlockedService = userBlockedService;
+    }
 
 
     @Operation(summary = "Удаление данного пользователя из блока у авторизированного пользователя")
@@ -44,12 +46,14 @@ public class UserBlockedResourceController {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
 
-            ;
+            userBlockedService.deleteUserFromBlockById(user.getId(),userId);
             return new ResponseEntity<>("Пользователь с id = " + userId + " был успешно удален", HttpStatus.OK);
         }
+
+
 //        return new ResponseEntity<>("Закладка с id = " + questionId + " не существует", HttpStatus.BAD_REQUEST);
     }
 
 
 
-}
+
