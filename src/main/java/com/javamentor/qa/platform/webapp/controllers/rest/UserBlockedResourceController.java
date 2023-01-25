986309b47236b082aca850507700853d7def0984
xenklao <109021940+xenklao@users.noreply.github.com>
2023-01-25 19:27:05 +0500

    comment getAllUserBookMarkGroupNamesByUserId

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserBlockedResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserBlockedResourceController.java
@@ -1,44 +1,47 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
-import com.javamentor.qa.platform.models.dto.UserBlockDto;
+
+import com.javamentor.qa.platform.models.entity.bookmark.BookMarks;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.dto.UserBlockDtoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user/")
-@Tag(name = "User Blocked Resource Controller", description = "Управление заблокированными сущностями")
 public class UserBlockedResourceController {
-
-    private final UserBlockDtoService userBlockDtoService;
-
-    public UserBlockedResourceController(UserBlockDtoService userBlockDtoService) {
-        this.userBlockDtoService = userBlockDtoService;
+    @Operation(summary = "Удаление данного пользователя из блока у авторизированного пользователя")
+    @ApiResponse(responseCode = "200",
+            description = "Пользователь удален",
+            content = {
+                    @Content(
+                            mediaType = "application/json"
+                    )
+            })
+    @ApiResponse(responseCode = "400",
+            description = "Пользователь с таким userId не существует",
+            content = {
+                    @Content(
+                            mediaType = "application/json"
+                    )
+            })
+    @DeleteMapping("/bookmark/{id}")
+    public ResponseEntity<?> deleteUserByUserId(@PathVariable("id") @RequestBody Long questionId){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<BookMarks> bookmarks = bookmarksService.getBookmarkByQuestionIdAndUserId(user.getId(), questionId);
+        if (bookmarks.isPresent()) {
+            bookmarksService.deleteById(questionId);
+            return new ResponseEntity<>("Закладка с id = " + questionId + " была успешно удалена", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Закладка с id = " + questionId + " не существует", HttpStatus.BAD_REQUEST);
     }
 
-    @Operation(summary = "Получение списка заблокированных пользователей у авторизированного пользователя.",
-            description = "Получение списка заблокированных пользователей у авторизированного пользователя.")
-    @ApiResponse(responseCode = "200", description = "Список забокированных пользователей выведен", content = {
-            @Content(mediaType = "application/json")
-    })
-    @ApiResponse(responseCode = "400", description = "Список забокированных пользователей не выведен", content = {
-            @Content(mediaType = "application/json")
-    })
-    @GetMapping("/block")
-    public ResponseEntity<List<UserBlockDto>> getAllBlockedUsers(@AuthenticationPrincipal User user){
-        return new ResponseEntity<>(userBlockDtoService.getAllBlockedUsers(user.getId()), HttpStatus.OK);
 
-    }
 
 }
