e5c19f45319a95cdd82869e43e5914522c5d8803
xenklao <109021940+xenklao@users.noreply.github.com>
2023-01-27 20:31:40 +0500

    fixing block

--- src/main/java/com/javamentor/qa/platform/service/impl/model/BlockChatUserListServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/BlockChatUserListServiceImpl.java
@@ -4,6 +4,7 @@ import com.javamentor.qa.platform.dao.abstracts.model.BlockChatUserListDao;
 import com.javamentor.qa.platform.models.entity.chat.BlockChatUserList;
 import com.javamentor.qa.platform.service.abstracts.model.BlockChatUserListService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -11,8 +12,9 @@ public class BlockChatUserListServiceImpl extends ReadWriteServiceImpl<BlockChat
 
     private final BlockChatUserListDao blockChatUserListDao;
 
-
-    void deleteUserFromBlockById(Long profile, Long blocked){
+    @Transactional
+    @Override
+    public void deleteUserFromBlockById(Long profile, Long blocked){
         blockChatUserListDao.deleteUserFromBlockById(profile,blocked);
     }
     public BlockChatUserListServiceImpl(BlockChatUserListDao blockChatUserListDao) {
