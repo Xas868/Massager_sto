ba69c7b9a2da1b9d0bf2dd01ccd648118bfbc6dd
xenklao <109021940+xenklao@users.noreply.github.com>
2023-01-27 20:11:00 +0500

    replacement

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/BlockChatUserListDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/model/BlockChatUserListDao.java
@@ -3,4 +3,7 @@ package com.javamentor.qa.platform.dao.abstracts.model;
 import com.javamentor.qa.platform.models.entity.chat.BlockChatUserList;
 
 public interface BlockChatUserListDao extends ReadWriteDao<BlockChatUserList, Long>{
+
+
+    void deleteUserFromBlockById(Long profile, Long block);
 }
