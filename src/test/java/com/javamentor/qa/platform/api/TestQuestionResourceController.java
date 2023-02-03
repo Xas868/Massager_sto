b0e1d2365dd3938db1e23e83773351eee063150d
xenklao <109021940+xenklao@users.noreply.github.com>
2023-02-03 22:16:58 +0500

    initial commit

---
+++ src/test/java/com/javamentor/qa/platform/api/TestQuestionResourceController.java
@@ -0,0 +1,50 @@
+package com.javamentor.qa.platform.api;
+
+
+import com.javamentor.qa.platform.AbstractClassForDRRiderMockMVCTests;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TestQuestionResourceController extends AbstractClassForDRRiderMockMVCTests {
+
+
+//получение пагинированного списка
+@Test
+@Sql(scripts = "/script/TestQuestionResourceController/allQuestionsWithTrackedTagsAndIgnoredTags/Before.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/script/TestQuestionResourceController/allQuestionsWithTrackedTagsAndIgnoredTags/After.sql",
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+void allQuestionsWithTrackedTagsAndIgnoredTags() throws Exception {
+    mockMvc.perform(get("/api/user/question")
+                    .param("page","1")
+                    .content("")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", "Bearer " + getToken("user101@mail.ru", "user101"))
+            )
+            .andDo(print())
+            .andExpect(status().isOk());
+
+
+//            .andExpect(jsonPath("$.items.[0].questionId", Is.is(101)))
+//            .andExpect(jsonPath("$.items.[0].title", Is.is("Question 101")))
+//
+//            .andExpect(jsonPath("$.items.[0].listTagDto.size()", Is.is(1)))
+//            .andExpect(jsonPath("$.items.[0].listTagDto[0].id", Is.is(101)))
+//            .andExpect(jsonPath("$.items.[0].listTagDto[0].name", Is.is("iThKcj2")))
+//            .andExpect(jsonPath("$.items.[0].listTagDto[0].description", Is.is("Description of tag 2")))
+//
+//            .andExpect(jsonPath("$.items.[0].countAnswer", Is.is(1)))
+//            .andExpect(jsonPath("$.items.[0].view", Is.is(1)))
+//            .andExpect(jsonPath("$.items.[0].vote", Is.is(5)))
+
+
+}
+
+}
---
+++ src/test/resources/script/TestQuestionResourceController/allQuestionsWithTrackedTagsAndIgnoredTags/After.sql
@@ -0,0 +1,8 @@
+TRUNCATE user_entity cascade;
+TRUNCATE role CASCADE;
+TRUNCATE tag CASCADE;
+TRUNCATE question CASCADE;
+truncate question_has_tag cascade;
+truncate reputation cascade;
+truncate votes_on_questions cascade;
+truncate question_viewed cascade;
