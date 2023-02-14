5eac28c4eaa895db375aec11422f0f7cdb8e7e80
evgenii.kazmin <varenkine@gmail.com>
2023-02-14 12:14:00 +0300

    fix(API QuestionResourceController): в markQuestionLikeViewed() теперь @PostMapping вместо @GetMapping, добавлены тесты

---
+++ src/test/resources/script/TestQuestionResourceController/postMarkQuestionLikeViewed/After.sql
@@ -0,0 +1,17 @@
+
+TRUNCATE role CASCADE;
+
+TRUNCATE user_entity CASCADE;
+
+TRUNCATE question CASCADE;
+
+TRUNCATE answer CASCADE;
+
+TRUNCATE tag CASCADE;
+
+TRUNCATE question_has_tag CASCADE;
+
+TRUNCATE reputation CASCADE;
+
+
+
