9b67fe29c2009d4cfc3aebe52ae9b2420e256d13
Реализовать_api_вывод _ответов_за неделю <predator.1199@mail.ru>
2023-05-28 14:50:56 +0300

    add property in MyTestPj commit

--- src/main/java/com/javamentor/qa/platform/MyTestPj.java
+++ src/main/java/com/javamentor/qa/platform/MyTestPj.java
@@ -1,8 +1,20 @@
 package com.javamentor.qa.platform;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyTestPj {
 
     public static void main(String[] args) {
         System.out.println("_________");
     }
+
+    private String firstName;
+    private String secondName;
+    private int age;
+
 }
